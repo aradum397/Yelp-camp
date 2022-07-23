64b15e372e68dd8b8b5d445b327ecd4f7026bca7
aradum397 <soyshpardt@gmail.com>
2022-07-23 14:44:42 +0300

    added start script; changed port

--- index.js
+++ index.js
@@ -110,6 +110,8 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { err });
 });
 
-app.listen(3000, () => {
-    console.log('Now listening on port 3000!');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}!`);
 });
