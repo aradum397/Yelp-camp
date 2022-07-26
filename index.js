9217e2d93589446c3d823a7c694a038dd0ca6a60
aradum397 <soyshpardt@gmail.com>
2022-07-26 12:27:45 +0300

    seed database

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ const users = require('./routes/users');
 const { authenticate } = require('passport');
 const { isLoggedin } = require('./middleware');
 
-const dbUrl = process.env.DB_URL
-    // || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl =  process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true
